443fd23691439b9eecfdf1b49b979bd49aad987e
Miquel Brazil <miquel@brazilliance.co>
2024-03-07 23:45:40 -0500

    fix: Width margins for responsive layout
    Fix width margins for responsive layout.

--- site/interface/Components/Media/Form/AddMediaForm.php
+++ site/interface/Components/Media/Form/AddMediaForm.php
@@ -1,5 +1,5 @@
-<div x-data="dropzone">
-	<form x-show="State.uploadStart" action="/api/v0/media/upload" method="POST" enctype="multipart/form-data" class="flex flex-col p-8 bg-white rounded-lg gap-4 w-full md:w-96 h-dvh md:h-auto" @change="updateActions" @submit.prevent="uploadMedia"
+<div x-data="dropzone" class="w-full md:w-96">
+	<form x-show="State.uploadStart" action="/api/v0/media/upload" method="POST" enctype="multipart/form-data" class="flex flex-col p-8 bg-white rounded-lg gap-4 h-dvh md:h-auto" @change="updateActions" @submit.prevent="uploadMedia"
 		x-transition:leave="transition ease-in duration-300"
         x-transition:leave-start="opacity-100 scale-100"
         x-transition:leave-end="opacity-0 scale-90"
@@ -71,7 +71,7 @@
 			<span x-text="State.uploading.label"></span>
 		</button>
 	</form>
-	<div x-show="State.uploadSuccess" class="flex flex-col p-8 bg-white rounded-lg gap-4 w-full md:w-96 h-dvh md:h-auto justify-center items-center"
+	<div x-show="State.uploadSuccess" class="flex flex-col p-8 bg-white rounded-lg gap-4 justify-center items-center"
 		x-transition:enter="transition ease-out duration-300"
         x-transition:enter-start="opacity-0 scale-90"
         x-transition:enter-end="opacity-100 scale-100"
